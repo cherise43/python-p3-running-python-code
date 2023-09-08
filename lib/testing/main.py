249071db94918bf6bdb8d091aa6e2bde83168df3
Cherise <nashpaiessy@gmail.com>
2023-09-09 00:41:06 +0300

    done

---
+++ lib/testing/main.py
@@ -0,0 +1,20 @@
+def sudoapt (val1,val2):
+     return val1 + val2
+sudoapt(5,4)
+
+if 5>4 :
+    print("less than")
+
+    std2 =[20,30,45,60,45]
+    total=0
+   # print(grade)
+for grade in std2:
+    perc=grade*2
+    print("Before",total)
+    total+= perc 
+    print("After",total)
+
+studentLIst=["mercy","JJ,Lynn"]
+for name in studentLIst:
+ print ("Is a Moringa School Student")
+
